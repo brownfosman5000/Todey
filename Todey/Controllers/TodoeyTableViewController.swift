127c0a235db61598f76534e77616c72b06edc4b0
Foster Brown <fosterbrown@fosters-imac.nyc.rr.com>
2018-03-31 17:11:37 -0400

    User Defaults and Codables

--- Todey/Controllers/TodoeyTableViewController.swift
+++ Todey/Controllers/TodoeyTableViewController.swift
@@ -12,29 +12,17 @@ class TodoeyTableViewController: UITableViewController {
 
     var items = [Item]()
     let defaults = UserDefaults.standard
-    
+    let itemsFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last?.appendingPathComponent("Items.plist")
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadItems()
         
-        if let itemArray = defaults.array(forKey: "TodeyList") as? [Item]{
-            items = itemArray
-        }
-        let newItem = Item()
-        newItem.itemName = "Make me a sandwhich"
-        items.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.itemName = "Test Me"
-        items.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.itemName = "Get the oatmeal"
-        items.append(newItem3)
-        
-        let newItem4 = Item()
-        newItem4.itemName = "Get me milk"
-        items.append(newItem4)
+//        if let itemArray = defaults.array(forKey: "TodeyList") as? [Item]{
+//            items = itemArray
+//        }
+
 
     }
 
@@ -59,30 +47,18 @@ class TodoeyTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
         return items.count
     }
     
     
     //MARK: - Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-    
         tableView.deselectRow(at: indexPath, animated: true)
 
         items[indexPath.row].done = !items[indexPath.row].done
-        
-        
-        
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
-
         tableView.reloadData()
-//        if tableView.cellForRow(at: indexPath)?.accessoryType != .checkmark{
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//        }
-//        else{
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//        }
     }
     
     
@@ -103,7 +79,9 @@ class TodoeyTableViewController: UITableViewController {
             
             self.tableView.reloadData()
             
-            self.defaults.set(self.items, forKey: "TodeyList")
+            self.saveItems()
+            
+            //self.defaults.set(self.items, forKey: "TodeyList")
         }
         alert.addTextField { (textField) in
             gtextField = textField
@@ -113,6 +91,31 @@ class TodoeyTableViewController: UITableViewController {
 
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(self.items)
+            try data.write(to: self.itemsFilePath!)
+        }
+        catch{
+            print("Problem encoding data: \(error)")
+        }
+    }
+    
+    func loadItems(){
+        if let data = try? Data(contentsOf: itemsFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+                items = try decoder.decode([Item].self, from: data)
+                
+            }
+            catch{
+                print("Failed to decode: \(error)")
+            }
+        }
+
     }
     
 
